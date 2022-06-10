30eae215b869af002a644f5d0754f07fead76889
courtneyhaas <95246631+courtneyhaas@users.noreply.github.com>
2022-06-10 11:06:49 -0400

    Update Final Project Code.R

--- Final Project Code.R
+++ Final Project Code.R
@@ -7,35 +7,28 @@ summary(d1)
 str(d1)
 
 
-## changing outcome to Yes and No for some analysis ##
+## changing outcome to Yes and No for analysis ##
 d1$Outcome <- as.character(d1$Outcome)
 
 
-    ##Checking Correlations##
+ ## Checking Correlations ##
 library(GGally)
 
 d1_sub <- d1 %>%
   dplyr::select(Pregnancies,Glucose, BloodPressure, 
                 SkinThickness, Insulin, BMI, Age)
 
-##########
 GGally::ggcorr(d1_sub, label = T) +
   ggtitle("Visualizing Correlation Matrix as a Heatmap") +
   theme(plot.title=element_text(hjust=0.5))
 
-    ##Scatterplots##
+ ## Scatterplots ##
 GGally::ggpairs(d1_sub) +
   ggtitle("More Informative Scatterplot Matrix") + 
   theme(plot.title=element_text(hjust=0.5))
 
 
-###Insert Scatterplot######
-
-
-##############
-
-
-####BAR PLOT for Outcome
+## Bar Plot for Outcome ##
 ggplot(d1,aes(x=Outcome)) +
   geom_bar(fill = c("darkseagreen2", "darkseagreen")) +
   labs(x= "Outcome",
@@ -45,7 +38,7 @@ ggplot(d1,aes(x=Outcome)) +
   theme(plot.title=element_text(hjust=0.50))
 
 
-########Histogram before and after imputation#########
+## Histogram before and after imputation ##
 ggplot(diabetes) +
   geom_histogram(mapping=aes(x=BloodPressure),fill="lightskyblue2")+
   theme_light()+
@@ -62,27 +55,28 @@ labs(title = "Histogram for Blood Pressure After Imputation to Median",
      x = "Blood Pressure (mmHG)",
      y= "Count")+
   theme(plot.title=element_text(hjust=0.50))
-###################################################
 
 
+
+## Logistic Regression ##
 d1$Outcome <- as.numeric(d1$Outcome)
-##Actual Logistic Regression
+
 lmodel <- glm(Outcome ~ SkinThickness + Pregnancies + Glucose + BMI + Age + BloodPressure, data = d1, family = "binomial")
 
 summary(lmodel)
 
-#####ROC
+## ROC ##
 my_roc <- ROSE::roc.curve(lmodel$y,fitted(lmodel),plotit = T)
 my_roc 
 ##area under the curve .837
 
-#####Pseduo r^2
+## Pseduo r^2 ##
 DescTools::PseudoR2(lmodel,which="McKelveyZavoina")
 
 lmodel %>% 
   broom::tidy(conf.int=T,exponentiate=T)
 
-#interpreting Pregnancies: As Pregnancies increases by 1 point, the odds of having
+## Interpreting Pregnancies: As Pregnancies increases by 1 point, the odds of having
 ##diabetes increases by ((1.118-1)*100 = 11.8% ) 11.8%
 
 
@@ -106,14 +100,11 @@ plot
 library(plotly)
 plotly::ggplotly(plot)
 
+car::vif(lmodel) ## VIF good
 
 
-car::vif(lmodel) ##VIF good
-
-
-################
+## Creating training and testing sets ##
 set.seed(12345)
-
 d1$Outcome <- as.numeric(d1$Outcome)
 
 ##70% of data in training set: fit the model to training
@@ -121,37 +112,30 @@ d1$Outcome <- as.numeric(d1$Outcome)
 samp <- sample(c("train","test"), nrow(d1), replace = T, prob = c(.7, .3))
 
 d_train <- d1[samp == "train",] #only keep rows where samp = train
-
 d_test <- d1[samp == "test",] 
 
 testmodel <- glm(Outcome ~ SkinThickness + Pregnancies + Glucose + BMI + Age + BloodPressure, data = d_train, family = "binomial")
 
 
-
 ROSE::roc.curve(testmodel$y,fitted(testmodel),plotit = T) 
-##.840
+## 0.840
 
 d_test$pred_probs <- predict.glm(testmodel,newdata=d_test,type="response")
-
 pt <- table(d_test$Outcome,d_test$pred_probs > 0.50)
 pt1 <- addmargins(pt)
 
 write.csv(pt1, "sstable.csv",row.names = F)
 
-
-
-#####Specificity
-
+## Specificity ##
 pt[1,1]/sum(pt[1,])
-#We are 88.05% accurate when predicting someone to not have diabetes
+    #88.05% accurate when predicting someone to not have diabetes
 
-######Sensitivity
+## Sensitivity ##
 pt[2,2]/sum(pt[2,])
-##57% accurate when predicting someone to have diabetes
+    # 57% accurate when predicting someone to have diabetes
 
 
-
-      ### using different cut off points##
+## using different cut off points ##
 cp <- seq(0.20,0.95,by=0.01) #potential cutoff values
 sn <- vector("double",length(cp)) #sensitivity
 sp <- vector("double",length(cp)) #specificity
@@ -179,9 +163,7 @@ ssplot
 
 
 
-#################predicted probabilty graph?
-
-
+## predicted probabilty graph ##
 d2 <- d1 %>% 
   dplyr::mutate(BMIlevel = case_when(BMI <= 18.5 ~ '1',
                                     BMI <= 24.9 ~ '2',
@@ -190,18 +172,12 @@ d2 <- d1 %>%
 d3 <- d2 %>% 
   select("BMIlevel","Pregnancies","Glucose","Outcome")
 
-
 d3$BMIlevel <- factor(d3$BMIlevel)
-
 d3$BMIlevel1 <- relevel(d3$BMIlevel,ref="4")
-
 d3$BMIlevel1
-
 summary(d3)
 
-
 lmodel <- glm(Outcome ~  Pregnancies + Glucose + BMIlevel1 , data = d3, family = "binomial")
-
 
 new_d3 <- tibble(Pregnancies = rep(3,100), #repeats 3.5 100 times
                   BMIlevel1 = rep(1,100), #repeats 1 100 times
@@ -209,11 +185,9 @@ new_d3 <- tibble(Pregnancies = rep(3,100), #repeats 3.5 100 times
 new_d32 <- tibble(Pregnancies = rep(3,100),
                   BMIlevel1 = rep(2,100), #rank is at rank 2
                   Glucose = seq(200,800,length.out = 100))
-
 new_d33 <- tibble(Pregnancies = rep(3,100),
                   BMIlevel1 = rep(3,100),
                   Glucose = seq(200,800,length.out = 100))
-
 new_d34 <- tibble(Pregnancies = rep(3,100),
                   BMIlevel1 = rep(4,100),
                   Glucose = seq(200,800,length.out = 100))
